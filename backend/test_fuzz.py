319c5a9898676cd4945d355c312178c97e3f67ca
Taher <rangwalataher17@gmail.com>
2026-09-20 12:48:01 +0530

    git ignore

--- backend/test_fuzz.py
+++
@@ -1,7 +0,0 @@
-from rapidfuzz import process, fuzz
-
-product_names = ["Red Apple", "Green Apple", "Banana"]
-
-# What if user types just "Apple" twice?
-match1 = process.extractOne("Apple", product_names, scorer=fuzz.WRatio, score_cutoff=70)
-print(match1)
